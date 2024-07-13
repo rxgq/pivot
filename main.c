43fb5e73438699908f5010375bfa3c6c74bcbec0
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-13 18:39:35 +0100

    added create window

--- main.c
+++ main.c
@@ -1,5 +1,59 @@
-#include <stdio.h>
+#include <windows.h>
+
+typedef struct {
+    const char* title;
+    int height;
+    int width;
+} RomaWindow;
+
+HWND create_window(HINSTANCE hInstance, const RomaWindow *window) {
+    WNDCLASSEX wc = {0};
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc   = DefWindowProc;
+    wc.hInstance     = hInstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszClassName = window->title;
+
+    if (!RegisterClassEx(&wc)) {
+        MessageBox(NULL, "Window Registration Failed!", "Error", MB_ICONEXCLAMATION | MB_OK);
+        return NULL;
+    }
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        window->title,
+        window->title,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        window->width, window->height,
+        NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL) {
+        MessageBox(NULL, "Window Creation Failed!", "Error", MB_ICONEXCLAMATION | MB_OK);
+        return NULL;
+    }
+
+    ShowWindow(hwnd, SW_SHOWNORMAL);
+    UpdateWindow(hwnd);
+
+    return hwnd;
+}
 
 int main() {
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+
+    RomaWindow window = {"roguelike", 512, 512};
+    HWND hwnd = create_window(hInstance, &window);
+
+
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
