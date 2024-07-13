92a9572895a10a5329461af19514bccc96e60ed4
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-13 19:42:40 +0100

    added text positioning

--- main.c
+++ main.c
@@ -4,9 +4,22 @@ typedef struct {
     const char* title;
     int height;
     int width;
+    HWND hwnd;
 } RomaWindow;
 
-HWND create_window(HINSTANCE hInstance, const RomaWindow *window) {
+RomaWindow* create_window(char *title, int height, int width) {
+    RomaWindow *window = malloc(sizeof(RomaWindow));
+    if (!window) {
+        MessageBox(NULL, "Memory Allocation Failed!", "Error", MB_ICONEXCLAMATION | MB_OK);
+        return NULL;
+    }
+
+    window->title = title;
+    window->height = height;
+    window->width = width;
+
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+
     WNDCLASSEX wc = {0};
     wc.cbSize        = sizeof(WNDCLASSEX);
     wc.lpfnWndProc   = DefWindowProc;
@@ -21,7 +34,7 @@ HWND create_window(HINSTANCE hInstance, const RomaWindow *window) {
         return NULL;
     }
 
-    HWND hwnd = CreateWindowEx(
+    window->hwnd = CreateWindowEx(
         0,
         window->title,
         window->title,
@@ -30,35 +43,99 @@ HWND create_window(HINSTANCE hInstance, const RomaWindow *window) {
         window->width, window->height,
         NULL, NULL, hInstance, NULL);
 
-    if (hwnd == NULL) {
+    if (window->hwnd == NULL) {
         MessageBox(NULL, "Window Creation Failed!", "Error", MB_ICONEXCLAMATION | MB_OK);
         return NULL;
     }
 
-    ShowWindow(hwnd, SW_SHOWNORMAL);
-    UpdateWindow(hwnd);
+    ShowWindow(window->hwnd, SW_SHOWNORMAL);
+    UpdateWindow(window->hwnd);
 
-    return hwnd;
+    return window;
 }
 
-void draw_text(HWND hwnd, char *text) {
+void draw_text(RomaWindow *window, char *text) {
     RECT bounds;
-    GetClientRect(hwnd, &bounds);
+    GetClientRect(window->hwnd, &bounds);
 
-    HDC hdc = GetDC(hwnd);
+    HDC hdc = GetDC(window->hwnd);
+    DrawText(hdc, text, -1, &bounds, DT_LEFT);
 
-    DrawText(hdc, text, -1, &bounds, DT_CENTER | DT_VCENTER);
+    ReleaseDC(window->hwnd, hdc);
+}
 
-    ReleaseDC(hwnd, hdc);
+void draw_text_from_top_left(RomaWindow *window, char *text, int t, int l) {
+    RECT bounds;
+    GetClientRect(window->hwnd, &bounds);
+
+    bounds.top = t;
+    bounds.left = l;
+
+    HDC hdc = GetDC(window->hwnd);
+    DrawText(hdc, text, -1, &bounds, DT_LEFT);
+
+    ReleaseDC(window->hwnd, hdc);
+}
+
+void draw_text_from_top_right(RomaWindow *window, char *text, int t, int r) {
+    RECT bounds;
+    GetClientRect(window->hwnd, &bounds);
+
+    HDC hdc = GetDC(window->hwnd);
+
+    SIZE textSize;
+    GetTextExtentPoint32(hdc, text, strlen(text), &textSize);
+
+    bounds.top = t;
+    bounds.right = bounds.right - r;
+    bounds.left = bounds.right - textSize.cx;
+
+    DrawText(hdc, text, -1, &bounds, DT_LEFT);
+
+    ReleaseDC(window->hwnd, hdc);
+}
+
+void draw_text_from_bottom_left(RomaWindow *window, char *text, int b, int l) {
+    RECT bounds;
+    GetClientRect(window->hwnd, &bounds);
+
+    HDC hdc = GetDC(window->hwnd);
+
+    SIZE textSize;
+    GetTextExtentPoint32(hdc, text, strlen(text), &textSize);
+
+    bounds.left = l;
+    bounds.bottom = bounds.bottom - b;
+    bounds.top = bounds.bottom - textSize.cy;
+
+    DrawText(hdc, text, -1, &bounds, DT_LEFT);
+
+    ReleaseDC(window->hwnd, hdc);
+}
+
+
+void draw_text_from_bottom_right(RomaWindow *window, char *text, int b, int r) {
+    RECT bounds;
+    GetClientRect(window->hwnd, &bounds);
+
+    HDC hdc = GetDC(window->hwnd);
+
+    SIZE textSize;
+    GetTextExtentPoint32(hdc, text, strlen(text), &textSize);
+
+    bounds.right = bounds.right - r;
+    bounds.bottom = bounds.bottom - b;
+    bounds.top = bounds.bottom - textSize.cy;
+    bounds.left = bounds.right - textSize.cx;
+
+    DrawText(hdc, text, -1, &bounds, DT_LEFT);
+
+    ReleaseDC(window->hwnd, hdc);
 }
 
 int main() {
-    HINSTANCE hInstance = GetModuleHandle(NULL);
-
-    RomaWindow window = {"roguelike", 512, 512};
-    HWND hwnd = create_window(hInstance, &window);
-    draw_text(hwnd, "Hello World!");
-
+    RomaWindow *window = create_window("Roguelike", 512, 512);
+    draw_text_from_bottom_left(window, "MY_TasdasdEXT_STRadsasdasdasING", 100, 10);
 
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0) > 0) {
