1fb54eaef12fb0f9c96c5718f17eedd66cb4d9e1
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-19 16:31:39 +0100

    clean up

--- out/output.c
+++ out/output.c
@@ -3,15 +3,26 @@
 #include <string.h>
 
 int fib(int n){
+if(n<=0){
+return 0;}
+if(n==1){
+printf(0);
+return 0;}
+if(n==2){
+printf(0);
+printf(1);
+return 1;}
 int a=0;
-int b=0;
-while(b<n){
+int b=1;
+int count=2;
+while(count<n){
 int temp=a+b;
 a=b;
 b=temp;
 printf(temp);
+count=count+1;
 }
-return a;}
+return b;}
 
 int main(int argc,char*argv[]){
 fib(10);return 0;
